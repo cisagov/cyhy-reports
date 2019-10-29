8fbf1c331dfff7e3f49b342f8692c5e97b22e279
Dave Redmin <david.redmin@beta.dhs.gov>
2019-10-29 16:15:52 -0400

    Blacken code

--- setup.py
+++ setup.py
@@ -6,40 +6,46 @@ def package_files(directory):
     paths = []
     for (path, directories, filenames) in os.walk(directory):
         for filename in filenames:
-            paths.append('../' + os.path.join(path, filename))
+            paths.append("../" + os.path.join(path, filename))
     return paths
 
 
-extra_files = package_files('cyhy_report/assets')
+extra_files = package_files("cyhy_report/assets")
 
 setup(
-    name='cyhy-reports',
-    version='0.0.2',
-    author='Mark Feldhousen Jr.',
-    author_email='mark.feldhousen@hq.dhs.gov',
-    packages=['cyhy_report',
-              'cyhy_report.bod_scorecard',
-              'cyhy_report.customer',
-              'cyhy_report.cybex_scorecard',
-              'cyhy_report.cyhy_notification',
-              'cyhy_report.scorecard',
-              'cyhy_report.m1513_scorecard'],
-    package_data={'cyhy_report.customer': ['*.mustache'],
-                  'cyhy_report.bod_scorecard': ['*.mustache', '*.js'],
-                  'cyhy_report.cybex_scorecard': ['*.mustache', '*.js'],
-                  'cyhy_report.cyhy_notification': ['*.mustache'],
-                  'cyhy_report.scorecard': ['*.mustache'],
-                  'cyhy_report.m1513_scorecard': ['*.mustache', '*.js'],
-                  '': extra_files},
-    scripts=['bin/cyhy-report',
-             'bin/cyhy-scorecard',
-             'bin/cyhy-bod-scorecard',
-             'bin/cyhy-cybex-scorecard',
-             'bin/cyhy-m1513-scorecard',
-             'bin/cyhy-notification'],
+    name="cyhy-reports",
+    version="0.0.2",
+    author="Mark Feldhousen Jr.",
+    author_email="mark.feldhousen@hq.dhs.gov",
+    packages=[
+        "cyhy_report",
+        "cyhy_report.bod_scorecard",
+        "cyhy_report.customer",
+        "cyhy_report.cybex_scorecard",
+        "cyhy_report.cyhy_notification",
+        "cyhy_report.scorecard",
+        "cyhy_report.m1513_scorecard",
+    ],
+    package_data={
+        "cyhy_report.customer": ["*.mustache"],
+        "cyhy_report.bod_scorecard": ["*.mustache", "*.js"],
+        "cyhy_report.cybex_scorecard": ["*.mustache", "*.js"],
+        "cyhy_report.cyhy_notification": ["*.mustache"],
+        "cyhy_report.scorecard": ["*.mustache"],
+        "cyhy_report.m1513_scorecard": ["*.mustache", "*.js"],
+        "": extra_files,
+    },
+    scripts=[
+        "bin/cyhy-report",
+        "bin/cyhy-scorecard",
+        "bin/cyhy-bod-scorecard",
+        "bin/cyhy-cybex-scorecard",
+        "bin/cyhy-m1513-scorecard",
+        "bin/cyhy-notification",
+    ],
     # url='http://pypi.python.org/pypi/CyHy/',
-    license='LICENSE.txt',
-    description='Reporting components for Cyber Hygiene',
+    license="LICENSE.txt",
+    description="Reporting components for Cyber Hygiene",
     # long_description=open('README.txt').read(),
     install_requires=[
         "cyhy-core >= 0.0.2",
@@ -55,6 +61,6 @@ setup(
         "docopt >= 0.6.2",
         "unicodecsv >= 0.9.4",
         "pyPdf >= 1.13",
-        "requests >= 2.21.0"
-    ]
+        "requests >= 2.21.0",
+    ],
 )
