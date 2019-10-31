7266ca6e2b8062461d147e736c6a6db7581170c1
Dave Redmin <david.redmin@beta.dhs.gov>
2019-10-31 14:24:24 -0400

    Make some blackened code slightly less ugly

--- extras/create_snapshots_reports_scorecard.py
+++ extras/create_snapshots_reports_scorecard.py
@@ -443,10 +443,13 @@ def sync_all_tallies(db):
 
 
 def pause_commander(db):
-    PAUSE_ITERATION_LIMIT = 30  # number of iterations to wait before giving up
-    PAUSE_ITERATION_WAIT_SECONDS = (
-        60
-    )  # number of seconds to wait between each check to see if the commander has paused
+    # number of iterations to wait before giving up
+    PAUSE_ITERATION_LIMIT = 30
+
+    # number of seconds to wait between each check to see
+    # if the commander has paused
+    PAUSE_ITERATION_WAIT_SECONDS = 60
+
     pause_iteration_count = 0
     ch = CHDatabase(db)
     doc = ch.pause_commander("create_snapshots_reports_scorecard", "report generation")
