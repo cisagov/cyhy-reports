ded95b5f77261569d44326f63a91ca9bf3f218c3
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2025-07-17 15:13:02 -0400

    Align versions of cyhy-reports package, cyhy-report script, and cyhy-cybex-scorecard script to match previously-updated version of cyhy-notification script
    All are now v1.1.0.

--- cyhy_report/cybex_scorecard/generate_cybex_scorecard.py
+++ cyhy_report/cybex_scorecard/generate_cybex_scorecard.py
@@ -3411,7 +3411,7 @@ def generate_empty_scorecard_json():
     return to_json(result)
 
 def main():
-    args = docopt(__doc__, version='v0.0.1')
+    args = docopt(__doc__, version='v1.1.0')
 
     if args['--generate-empty-scorecard-json']:
         print generate_empty_scorecard_json()
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_files = package_files("cyhy_report/assets")
 
 setup(
     name="cyhy-reports",
-    version="0.0.2",
+    version="1.1.0",
     author="Mark Feldhousen Jr.",
     author_email="mark.feldhousen@hq.dhs.gov",
     packages=[
