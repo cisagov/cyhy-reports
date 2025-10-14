bcd65c47f0e95b6c074e2120cb47c0c3aec4ced9
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2025-10-14 13:09:20 -0400

    Bump up the numbers of snapshot and report threads
    We are using a larger instance type now, so we need to do this to take
    advantage of the larger memory and number of processors.
    
    Co-authored-by: David Redmin <david.redmin@gwe.cisa.dhs.gov>

--- extras/create_snapshots_reports_scorecard.py
+++ extras/create_snapshots_reports_scorecard.py
@@ -40,8 +40,8 @@ current_time = util.utcnow()
 
 LOGGING_LEVEL = logging.INFO
 LOG_FILE = "snapshots_reports_scorecard_automation.log"
-REPORT_THREADS = 40
-SNAPSHOT_THREADS = 40
+REPORT_THREADS = 70
+SNAPSHOT_THREADS = 70
 
 NCATS_DHUB_URL = "dhub.ncats.cyber.dhs.gov:5001"
 
