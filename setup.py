a90c26e0a5f567a4258423f4ed4a9bd3d6e519a9
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-03-20 12:29:11 -0400

    Bump version from 1.1.2 to 1.1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ extra_files = package_files("cyhy_report/assets")
 
 setup(
     name="cyhy-reports",
-    version="1.2.0",
+    version="1.1.3",
     author="Mark Feldhousen Jr.",
     author_email="mark.feldhousen@hq.dhs.gov",
     packages=[
