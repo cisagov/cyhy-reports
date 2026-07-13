efeee4b23d971c1779152bb57b3128f0ef5287f8
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2026-07-13 14:29:08 -0400

    Bump version from 2.0.0 to 2.0.1
    Bump generate_report version from v2.0.0 to v2.0.1

--- cyhy_report/customer/generate_report.py
+++ cyhy_report/customer/generate_report.py
@@ -3640,7 +3640,7 @@ class ReportGenerator(object):
 
 
 def main():
-    args = docopt(__doc__, version="v2.0.0")
+    args = docopt(__doc__, version="v2.0.1")
     cyhy_db = database.db_from_config(args["--cyhy-section"])
     scan_db = database.db_from_config(args["--scan-section"])
 
